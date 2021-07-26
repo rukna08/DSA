35e5d815287a26515d4121b6a46464f81a179ae7
Ankur Banerjee <rukna2003@gmail.com>
2021-07-27 00:02:13 +0530

    ~

--- quicksort.c
+++ quicksort.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 
-void quicksort(){
-    
+void printarray(int* arr, size_t sizeofarr){
+    for(size_t i = 0; i < sizeofarr; i++){
+        printf("%d ", arr[i]);
+    }
+}
+void newline() {
+    printf("\n");
+}
+
+void quicksort(int* arr, size_t sizeofarr){
+    int pivot = sizeofarr - 1;
+    int i = -1;
+    int j = 1;
+    for(size_t k = 0; k < sizeofarr; k++){
+        if(arr[j] < arr[pivot]) {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            j++;
+        } else {
+            j++;
+        }
+    }
+    int temp = arr[pivot];
+    arr[pivot] = arr[j];
+    arr[j] = temp;
 }
 
 int main(){
+    int arr[] = {7, 2, 1, 8, 6, 3, 5, 4};
+    size_t sizeofarr = sizeof(arr)/sizeof(int);
 
+    printarray(arr, sizeofarr);
+    newline();
+    quicksort(arr, sizeofarr);
+    newline();
+    printarray(arr, sizeofarr);
+    newline();
+
+    return 0;
 }
