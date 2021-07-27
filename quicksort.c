03d082785002e8c3f136233a28b71e0250eee81a
Ankur Banerjee <rukna2003@gmail.com>
2021-07-27 21:27:54 +0530

    ~

--- quicksort.c
+++ quicksort.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+
+
+void printarray(int*, size_t);
+void quicksort(int*, int, int);
+
+
+
+int main(){
+    int arr[] = {7, 2, 1, 8, 6, 3, 5, 4};
+    size_t sizeofarr = sizeof(arr)/sizeof(int);
+    int right = (int)(sizeofarr - 1);
+
+    printarray(arr, sizeofarr);
+    quicksort(arr, 0, right);
+    printarray(arr, sizeofarr);
+
+    return 0;
+}
+
+
+
 void printarray(int* arr, size_t sizeofarr){
     printf("\n");
     for(size_t i = 0; i < sizeofarr; i++){
@@ -8,41 +29,30 @@ void printarray(int* arr, size_t sizeofarr){
     printf("\n");
 }
 
+
+
+void swap(int* arr, int i1, int i2) {
+    int temp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
+}
+
+
+
 void quicksort(int* arr, int left, int right){
-    int pivot = right - 1;
+    int pivot = right;
     int i = left-1;
     int j = left;
 
-    int ti = left-1;
-
-    for(size_t k = 0; k < right - 2; k++){
+    for(int k = 0; k < pivot; k++){
         if(arr[j] < arr[pivot]) {
             i++;
-
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
+            swap(arr, i, j);
             j++;
-            ti = i;
         } else {
             j++;
         }
     }
     
-    int temp = arr[pivot];
-    arr[pivot] = arr[ti+1];
-    arr[ti+1] = temp;
-}
-
-int main(){
-    int arr[] = {7, 2, 1, 8, 6, 3, 5, 4};
-    size_t sizeofarr = sizeof(arr)/sizeof(int);
-    int right = (int)sizeofarr;
-
-    printarray(arr, sizeofarr);
-    quicksort(arr, 0, right);
-    printarray(arr, sizeofarr);
-
-    return 0;
+    swap(arr, i+1, pivot);
 }
