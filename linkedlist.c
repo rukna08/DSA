1dfca4205fce6aa87b6caa44efcf6471e65969ef
rukna <46285633+rukna08@users.noreply.github.com>
2021-12-24 18:53:40 +0530

    `

--- linkedlist.c
+++ linkedlist.c
@@ -7,9 +7,12 @@
 
 // node structure.
 struct node {
+    
     int data;
     struct node* next;
+
 };
+
 typedef struct node node;
 
 
@@ -36,7 +39,83 @@ void insert(node** n, int data) {
             i->next = temp;
             return;
         }
+
+    }
+
+}
+
+
+
+
+
+// insertion of the first element in the list
+void inserthead(node** n, int data) {
+
+    node* t = malloc(sizeof(node));
+
+    t->data = data;
+    t->next = *n;
+    *n = t;
+
+}
+
+
+
+
+
+// insertion of the last element in the list
+void insertend(node** n, int data) {
+
+    for(node* i = *n; i != NULL; i = i->next) {
         
+        if(i->next == NULL) {
+            i->next = malloc(sizeof(node));
+            i->next->data = data;
+            i->next->next = NULL;
+            
+            return;
+        }
+
+    }
+
+}
+
+
+
+
+
+// insertion of the element in the specified position.
+void insertpos(node** n, int data, int position) {
+
+    int k = 1;
+    
+    for(node* i = *n; i != NULL; i = i->next) {
+
+        if(k == position-1) {
+
+            if(i->next == NULL) {
+                i->next = malloc(sizeof(node));
+                i->next->data = data;
+                i->next->next = NULL;
+
+                return;
+            }
+
+            // if(position == 1) {
+            //     inserthead(n, data);
+            //     return;
+            // }
+
+            node* t = malloc(sizeof(node));
+            t->data = data;
+            t->next = i->next;
+            i->next = t;
+            
+            return;
+        }
+        
+        k++;
+
     }
 
 }
@@ -47,6 +126,7 @@ void insert(node** n, int data) {
 
 // deletion of the whole list.
 void deletelist(node** n) {
+
     node* t1 = *n;
     node* t2;
 
@@ -57,35 +137,38 @@ void deletelist(node** n) {
     }
 
     *n = NULL;
+
 }
 
 
 
 
 
-// delete the first element of the list.
+// deletion of the first element in the list.
 void deletehead(node** n) {
+
     node* t1 = *n;
     *n = (*n)->next;
     free(t1);
+
 }
 
 
 
 
 
-// delete the last element of the list.
+// deletion of the last element in the list.
 void deletelast(node** n) {
 
     for(node* i = *n; i != NULL; i = i->next) {
-
+        
         if(i->next->next == NULL) {
             node* j = i->next;
             free(j);
             i->next = NULL;
             return;
         }
-    
+
     }
 
 }
@@ -93,8 +176,9 @@ void deletelast(node** n) {
 
 
 
-// delete the element in the specified position.
-void deleteatpos(node** n, int position) {
+// deletion of the element in the specified position.
+void deletepos(node** n, int position) {
+    
     int k = 1;
     
     for(node* i = *n; i != NULL; i = i->next) {
@@ -106,7 +190,9 @@ void deleteatpos(node** n, int position) {
         }
         
         k++;
+
     }
+
 }
 
 
@@ -115,12 +201,14 @@ void deleteatpos(node** n, int position) {
 
 // printing the whole list.
 void printlist(node* n) {
+
     if(n == NULL) {
         printf("List is empty.\n");
         return;
     }
 
     for(node* i = n; i != NULL; i = i->next) printf("%d\n", i->data);
+
 }
 
 
@@ -129,14 +217,12 @@ void printlist(node* n) {
 
 int main() {
     node* n = NULL;
-    node* n2 = NULL;
     
     insert(&n, 1);
     insert(&n, 2);
-    insert(&n, 3);
     insert(&n, 4);
-    
-    deleteatpos(&n, 3);
+
+    insertpos(&n, 99, 2);
 
     printlist(n);
 
