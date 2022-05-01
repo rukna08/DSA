85ecf0318f63e19cc6fb94b52a9a2025793b537b
rukna <46285633+rukna08@users.noreply.github.com>
2022-05-01 16:58:19 +0530

    -

---
+++ algorithms/selectionsort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void selectionsort(int* arr, size_t size) {
+    for(int i = 0; i < size; i++) {
+        int lowest = 2147483647;    // INT_MAX
+        int lowestIndex;
+
+        for(int j = i; j < size; j++) {
+            if(arr[j] < lowest) {
+                lowest = arr[j];
+                lowestIndex = j;
+            }
+        }
+
+        int temp = arr[i];
+        arr[i] = lowest;
+        arr[lowestIndex] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {55, 22, 11, 99, 56, 78, 2, 548};
+
+    size_t size = sizeof(arr) / sizeof(int);
+
+    selectionsort(arr, size);
+
+    for(int i = 0; i < size; i++) {
+        printf("%d\n", arr[i]);
+    }
+}
