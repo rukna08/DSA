02b5052b91bef18d870ad1cccb6f02d38ad93800
rukna <46285633+rukna08@users.noreply.github.com>
2022-05-06 15:05:08 +0530

    -

--- algorithms/selectionsort.c
+++ algorithms/selectionsort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <limits.h>
 
 void selectionsort(int* arr, size_t size) {
     for(int i = 0; i < size; i++) {
-        int lowest = 2147483647;    // INT_MAX
+        int lowest = INT_MAX;
         int lowestIndex;
 
         for(int j = i; j < size; j++) {
