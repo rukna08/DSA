4003e10888561dbcad82775e7c09aafb10dee081
rukna <46285633+rukna08@users.noreply.github.com>
2022-06-02 22:25:01 +0530

    -

--- algorithms/bubblesort.c
+++ algorithms/bubblesort.c
@@ -2,7 +2,7 @@
 
 void bubblesort(int* arr, size_t size) {
     for(int i = 0; i < size; i++) {
-        for(int j = 0; j < size - 1; j++) {
+        for(int j = 0; j < size - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
